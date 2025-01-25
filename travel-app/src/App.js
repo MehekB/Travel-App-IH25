1a3074f97d22112501df014fa5d165f3e808a5fa
Mehek Bhatnagar <mehekb@dhcp-172-24-204-093.mobile.uci.edu>
2025-01-24 21:49:23 -0800

    Added initial navbar to App.js

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <h1 className="logo">Travel App</h1>
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#transportation">Transportation</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </nav>  
     </div>
   );
 }
